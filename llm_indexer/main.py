98bbd5e11f95667149f70cd1e68c5b01a75fb4b1
Helder Oliveira <heldrida@gmail.com>
2023-11-15 21:35:30 +0000

    refactor: 💡 remove open ai debug, tweak prompt, etc

--- llm_indexer/main.py
+++ llm_indexer/main.py
@@ -7,23 +7,28 @@ from pathlib import Path
 from llama_index import download_loader, ServiceContext
 from llama_index.prompts import PromptTemplate
 from llama_index.llms import OpenAI
-import openai
 
-openai.log = "debug"
 app = FastAPI()
 loader = SitemapReader()
 
-llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo", log="debug")
+llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo")
 service_context = ServiceContext.from_defaults(llm=llm)
 
-# template = (
-#     "We have provided context information below. \n"
-#     "---------------------\n"
-#     "{context_str}"
-#     "\n---------------------\n"
-#     "Given this information, please answer the question and include the source url or documented loaded where the information comes from: {query_str}\n"
-# )
-# qa_template = PromptTemplate(template)
+MarkdownReader = download_loader("MarkdownReader")
+loader = MarkdownReader()
+
+template = (
+    "We have provided knowledge below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given the provided knowledge and not prior knowledge,"
+    "answer the query, including the commands and the documentation URL,\n"
+    "the answer should only contain accurate information from provided knowledge only\n"
+    "If no answer, ask to visit the main blog and documentation website\n"
+    "The query is: {query_str}\n"
+)
+qa_template = PromptTemplate(template)
 
 # docsUrls = [
 #   "https://docs.fleek.network/docs/node/install/",
@@ -45,21 +50,24 @@ def read_root():
 
 @app.get("/query")
 def query(question: Union[str, None] = None):
-  documents = SimpleWebPageReader(html_to_text=True).load_data(docsUrls)
+  # documents = SimpleWebPageReader(html_to_text=True).load_data(docsUrls)
   # documents = loader.load_data(sitemap_url='https://docs.fleek.network/sitemap.xml')
+  documents = loader.load_data(file=Path("./knowledge.md"))
 
   index = SummaryIndex.from_documents(documents, service_context=service_context)
   # index = TreeIndex.from_documents(documents)
 
   query_engine = index.as_query_engine()
-  # query_engine.update_prompts(
-  #   {"response_synthesizer:text_qa_template": qa_template}
-  # )
+  query_engine.update_prompts(
+    {"response_synthesizer:text_qa_template": qa_template}
+  )
 
   answer = query_engine.query(question)
 
   print("[debug] node.metadata:")
   print(answer.source_nodes[0].node.metadata)
+
+  print(answer)
 
   # return answer
   return { "answer": str(answer )}
