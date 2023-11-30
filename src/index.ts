15d146342447e586df134eb6c2e30961a2170708
Helder Oliveira <heldrida@gmail.com>
2023-11-30 17:43:52 +0000

    refactor: 💡 add docs url

--- src/index.ts
+++ src/index.ts
@@ -222,7 +222,7 @@ client.on("messageCreate", async (msg) => {
     msg.content.match(/.*[wW]h?en.*incentive[s]/)
   ) {
     msg.channel.send(
-      `👀 Hey ${msg.author.toString()}! We're working hard to make sure that the rewards mechanism is top-notch before we roll it out. Our team takes great care to deploy and test under the testnet, but it's important to note that the testnet is not incentivized. Therefore, rewards and incentives will only be available on the mainnet after passing rigorous tests. Rest assured, we're doing everything we can to make sure that you'll be rewarded for your efforts. Thank you for your patience and understanding!`,
+      `👀 Hey ${msg.author.toString()}! We're working hard to make sure that the rewards mechanism is top-notch before we roll it out. Our team takes great care to deploy and test under the testnet, but it's important to note that the testnet is not incentivized. Therefore, rewards and incentives will only be available on the mainnet after passing rigorous tests. Rest assured, we're doing everything we can to make sure that you'll be rewarded for your efforts. To learn more visit the documentation site https://docs.fleek.network, thanks for your patience and understanding!`,
     );
   }
 
