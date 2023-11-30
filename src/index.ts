a17acb17f8ca46a65f864fdeccb49be6d6b7eeb9
Helder Oliveira <heldrida@gmail.com>
2023-11-30 18:44:52 +0000

    refactor: 💡 add another regex for node working regex

--- src/index.ts
+++ src/index.ts
@@ -229,7 +229,10 @@ client.on("messageCreate", async (msg) => {
     );
   }
 
-  if (msg.content.match(/.*[iI]s.*(it|node).*working/gm)) {
+  if (
+    msg.content.match(/.*[iI]s.*(it|node).*working/gm) ||
+    msg.content.match(/.*[hH]ow.*if.*node.*working/gm)
+  ) {
     msg.channel.send(
       `👀 Hey ${msg.author.toString()}, to verify if your node is running correctly do a health checkup!
 
