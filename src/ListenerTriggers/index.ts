6d32766d861c457c0be381408408e5944dfe246d
Helder Oliveira <heldrida@gmail.com>
2023-12-05 18:58:40 +0000

    refactor: 💡 on gm/gn removal send msg to channel

--- src/ListenerTriggers/index.ts
+++ src/ListenerTriggers/index.ts
@@ -1,14 +1,17 @@
-import {
-  Message,
-  TextChannel,
-} from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import dayjs from "dayjs";
-import { whitelistNotRequired } from '../Messages/index.js';
-import assert from 'assert';
-import { deleteMsg, sendMsgToUser, sendMsgToChannel } from '../Utils/index.js';
+import { whitelistNotRequired } from "../Messages/index.js";
+import assert from "assert";
+import { deleteMsg, sendMsgToUser, sendMsgToChannel } from "../Utils/index.js";
 
-assert(process.env.WHITELIST_MSG_TIMEOUT_MINUTES, new Error('Oops! Missing WHITELIST_MSG_TIMEOUT_MINUTES env var'));
-assert(process.env.DISCORD_CHANNEL_ID_GM_GN, new Error('Oops! Missing DISCORD_CHANNEL_ID_GM_GN env var'));
+assert(
+  process.env.WHITELIST_MSG_TIMEOUT_MINUTES,
+  new Error("Oops! Missing WHITELIST_MSG_TIMEOUT_MINUTES env var"),
+);
+assert(
+  process.env.DISCORD_CHANNEL_ID_GM_GN,
+  new Error("Oops! Missing DISCORD_CHANNEL_ID_GM_GN env var"),
+);
 
 // App in-memory state
 let whiteListMsgCount = 0;
@@ -16,13 +19,14 @@ let lastWhiteListMsg = dayjs();
 let warningMsg: Message[] = [];
 
 type OnMessageCreate = {
-  expr: (msg: Message) => boolean,
-  cb: (msg: Message) => void,
-}
+  expr: (msg: Message) => boolean;
+  cb: (msg: Message) => void;
+};
 
 const whitelistQueries: OnMessageCreate = {
-  expr: (msg) => !!msg.content.includes("whitelist") ||
-  !!msg.content.match(/.*(form|application|apply|join).*(test|testnet)/gm),
+  expr: (msg) =>
+    !!msg.content.includes("whitelist") ||
+    !!msg.content.match(/.*(form|application|apply|join).*(test|testnet)/gm),
   cb: (msg) => {
     const currentWhiteListMsg = dayjs();
     const diffInMins = currentWhiteListMsg.diff(lastWhiteListMsg, "minute");
@@ -37,51 +41,62 @@ const whitelistQueries: OnMessageCreate = {
       lastWhiteListMsg = currentWhiteListMsg;
       whiteListMsgCount += 1;
     }
-  }
-}
+  },
+};
 
 const installSetupQueries: OnMessageCreate = {
-  expr: (msg) => !!msg.content.match(/.*([hH]ow|[cC]an).*(install|setup).*node/gm),
+  expr: (msg) =>
+    !!msg.content.match(/.*([hH]ow|[cC]an).*(install|setup).*node/gm),
   cb: (msg) => {
     msg.reply(
       // TODO: refactor/use a text message template instead
       `👀 Hey ${msg.author.toString()}, anyone can install and run node! Check the requirements and instructions in <https://docs.fleek.network/docs/node/install>`,
     );
   },
-}
+};
 
 const RolesQueries: OnMessageCreate = {
-  expr: (msg) => !!(msg.content.match(/.*[hH]ow.*(get|pick).*roles?/gm) || msg.content.match(/.*([wW]hat|[ww]here).*happen.*node.*role/gm) || msg.content.match(/.*([wW]hy|[wW]here|[hH]ad|[hH]ave).*role.*(delete|remove|lost|disappear|vanish)/gm) || msg.content.match(/.*([nN]o).*node.*role/gm)),
+  expr: (msg) =>
+    !!(
+      msg.content.match(/.*[hH]ow.*(get|pick).*roles?/gm) ||
+      msg.content.match(/.*([wW]hat|[ww]here).*happen.*node.*role/gm) ||
+      msg.content.match(
+        /.*([wW]hy|[wW]here|[hH]ad|[hH]ave).*role.*(delete|remove|lost|disappear|vanish)/gm,
+      ) ||
+      msg.content.match(/.*([nN]o).*node.*role/gm)
+    ),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
       `👀 Hey ${msg.author.toString()}, if you are looking for roles, go to <id:customize> to pick roles.`,
     );
   },
-}
+};
 
 const RewardIncentivesQueries: OnMessageCreate = {
-  expr: (msg) => !!(
-    msg.content.match(/.*[wW]h?en.*(reward|incentive|token)s?/gm) ||
-    msg.content.match(/.*[hH]ow.*get.*rewards?/gm) ||
-    msg.content.match(/.*([aA]re|[iI]s).*testnet.*incentiv(es|ised)/gm)
-  ),
+  expr: (msg) =>
+    !!(
+      msg.content.match(/.*[wW]h?en.*(reward|incentive|token)s?/gm) ||
+      msg.content.match(/.*[hH]ow.*get.*rewards?/gm) ||
+      msg.content.match(/.*([aA]re|[iI]s).*testnet.*incentiv(es|ised)/gm)
+    ),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
       `👀 Hey ${msg.author.toString()}, seems that you are talking about incentives or rewards? We're working hard to make sure that the rewards mechanism is top-notch before we roll it out. Our team takes great care to deploy and test under the testnet, but it's important to note that the testnet is not incentivized. Therefore, rewards and incentives will only be available on the mainnet after passing rigorous tests. Rest assured, we're doing everything we can to make sure that you'll be rewarded for your efforts. To learn more visit the documentation site https://docs.fleek.network, thanks for your patience and understanding!`,
     );
   },
-}
+};
 
 const NodeWorkingCorrectlyQueries: OnMessageCreate = {
-  expr: (msg) => !!(
-    msg.content.match(/.*[iI]s.*(it|node).*working/gm) ||
-    msg.content.match(/.*[hH]ow.*if.*node.*working/gm) ||
-    msg.content.match(/.*[iI]s.*it.*working.*properly/gm) ||
-    msg.content.match(/.*logs.*([oO][kK]|good|normal)/gm) ||
-    msg.content.match(/.*(normal|standard|correct).*logs/gm)
-  ),
+  expr: (msg) =>
+    !!(
+      msg.content.match(/.*[iI]s.*(it|node).*working/gm) ||
+      msg.content.match(/.*[hH]ow.*if.*node.*working/gm) ||
+      msg.content.match(/.*[iI]s.*it.*working.*properly/gm) ||
+      msg.content.match(/.*logs.*([oO][kK]|good|normal)/gm) ||
+      msg.content.match(/.*(normal|standard|correct).*logs/gm)
+    ),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
@@ -97,10 +112,11 @@ To learn more visit https://docs.fleek.network/docs/node/health-check
       `,
     );
   },
-}
+};
 
 const WatchLogsQueries: OnMessageCreate = {
-  expr: (msg) => !!(msg.content.match(/([hH]ow|[wW]hat).*(check|view|watch).*logs/gm)),
+  expr: (msg) =>
+    !!msg.content.match(/([hH]ow|[wW]hat).*(check|view|watch).*logs/gm),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
@@ -116,28 +132,26 @@ To learn more visit https://docs.fleek.network/docs/node/health-check
       `,
     );
   },
-}
+};
 
 const AskForHelpQueries: OnMessageCreate = {
-  expr: (msg) => !!(
-    msg.content.match(
+  expr: (msg) =>
+    !!msg.content.match(
       /.*[cC]an.*(someone|somebody|anyone|you|team).*help.*(me|please)?/gm,
-    )
-  ),
+    ),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
       `👀 Hey ${msg.author.toString()}, have you tried typing **!help** command in the channel to find the different ways to get help? If you have done that already, be patient, thank you!`,
     );
   },
-}
+};
 
 const AskNextTesnetPhaseQueries: OnMessageCreate = {
-  expr: (msg) => !!(
-    msg.content.match(
+  expr: (msg) =>
+    !!msg.content.match(
       /.*([wW]h?en|[wW]here|[wW]hat).*(next|test).*(phase|testnet)/gm,
-    )
-  ),
+    ),
   cb: (msg) => {
     // TODO: use text tmplt instead
     msg.reply(
@@ -148,14 +162,13 @@ Alternatively, you can keep visit our Blog site (<https://blog.fleek.network/>) 
 Thanks for your patience and understanding!`,
     );
   },
-}
+};
 
 const GreentingQueries: OnMessageCreate = {
-  expr: (msg) => !!(
-    ["gm", "gn"].some((greeting) =>
+  expr: (msg) =>
+    !!["gm", "gn"].some((greeting) =>
       msg.content.toLowerCase().startsWith(greeting),
-    )
-  ),
+    ),
   cb: async (msg) => {
     if (!msg.inGuild() || !msg.channel.isTextBased()) return;
 
@@ -172,26 +185,17 @@ const GreentingQueries: OnMessageCreate = {
       msg,
     });
 
-    const { author: user, channel } = msg;
     // TODO: use text tmplt instead
     const message = `${msg.author.toString()} for greetings use the channel <#${
       process.env.DISCORD_CHANNEL_ID_GM_GN
     }>`;
 
-    const hasSentMsg = await sendMsgToUser({
-      user,
+    await sendMsgToChannel({
+      channel: msg.channel as TextChannel,
       message,
-      channel: channel as TextChannel,
     });
-
-    if (!hasSentMsg) {
-      await sendMsgToChannel({
-        channel: channel as TextChannel,
-        message,
-      });
-    }
   },
-}
+};
 
 export const onMessageCreate: OnMessageCreate[] = [
   whitelistQueries,
@@ -203,4 +207,4 @@ export const onMessageCreate: OnMessageCreate[] = [
   AskForHelpQueries,
   AskNextTesnetPhaseQueries,
   GreentingQueries,
-]
+];
