1c0e175bd4ad1029d82ede8331542afc7d4687f4
Helder Oliveira <heldrida@gmail.com>
2023-12-08 18:20:29 +0000

    style: 💄 improve text style

--- src/Messages/index.ts
+++ src/Messages/index.ts
@@ -59,7 +59,13 @@ export const lookingForRoles = `👀 Hey $author, if you are looking for roles, 
 
 export const channelsRoles = `Channels and Roles`;
 
-export const lookingForRewards = `👀 Hey $author, seems that you are talking about incentives or rewards? We're working hard to make sure that the rewards mechanism is top-notch before we roll it out. Our team takes great care to deploy and test under the testnet, but it's important to note that the testnet is not incentivized. Therefore, rewards and incentives will only be available on the mainnet after passing rigorous tests. Rest assured, we're doing everything we can to make sure that you'll be rewarded for your efforts. To learn more visit the documentation site https://docs.fleek.network, thanks for your patience and understanding!`;
+export const lookingForRewards = `👀 Hey $author, seems that you are talking about incentives or rewards?
+
+We're working hard to make sure that the rewards mechanism is top-notch before we roll it out. Our team takes great care to deploy and test under the testnet, but it's important to note that the testnet is not incentivized. Therefore, rewards and incentives will only be available on the mainnet after passing rigorous tests.
+
+Rest assured, we're doing everything we can to make sure that you'll be rewarded for your efforts.
+
+To learn more visit the documentation site https://docs.fleek.network, thanks for your patience and understanding!`;
 
 export const incentivesRewards = `Incentives and Rewards`;
 
